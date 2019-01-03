90adf8698ea93a7b38e1767c508237a7a280330b
顾 <703172943@qq.com>
2019-01-03 22:25:04 +0800

    初始化顶部区域和底部区域

--- src/main.js
+++ src/main.js
@@ -1,15 +1,25 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+//入口文件
+// console.log('ok')
+
+//导入Vue模块
 import Vue from 'vue'
-import App from './App'
-import router from './router'
 
-Vue.config.productionTip = false
+//导入app 根组件
+import app from './App.vue'
 
-/* eslint-disable no-new */
-new Vue({
+//导入mint-ui 
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+Vue.use(MintUI)
+
+//导入 mui-ui 样式
+import '../lib/css/mui.css'
+
+//导入路由
+import router from './router/router'
+
+
+let vm = new Vue({
   el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+  render: c => c(app)
 })
--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,1 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
 
-Vue.use(VueRouter)
-
-export default new VueRouter({
-  routes: [
-
-  ]
-})
